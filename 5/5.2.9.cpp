4a7f3b9d78f6a14f2c65898ae498881ff2454c3b
Vasiliy Kulikov <67440493+SovietModernism@users.noreply.github.com>
2021-03-12 18:51:32 +0300

    solution was replaced with a new one

--- 5/5.2.9.cpp
+++ 5/5.2.9.cpp
@@ -1,128 +1,64 @@
-#include <iostream>
 #include <cstddef> // size_t
-#include <cstring> // strlen, strcpy
-
+#include <iostream>
+ 
 struct String {
-    String (const String &other) {
-        size = other.size;
-        str = new char[size + 1];
-        str[size] = '\0';
+    String(const char *str = "");
+    String(size_t n, char c);
+    ~String();
+ 
+    String(const String &other);
+    String &operator=(const String &other);
+ 
+    void append(const String &other);
+    
+    class PreparedString {
+    public:
+        void write(std::ostream& out) const;
         
-        const char * i = other.str;
-        while (i != other.str + other.size) {
-            str[i - other.str] = *i;
-            i++;
-        };
-    }
-    
-    String(const char *str = "") {
-        size = 0;
-        this->str = 0;
-        size = lengthOfStr(str);
-        this->str = (size > 0) ? cpStr(str, size) : 0;
-    }
-    
-    String(size_t n, char c) {
-        size = n;
-        str = new char[size + 1];
-        str[size] = '\0';
-        char * i = str;
-        while (i - str < size) {
-            *i = c;
-            i++;
-        };
-    }
-    
-    ~String() {
-        if (str != 0)
-            delete [] str;
-        str = 0;
-        size = 0;
-    }
-    
-    String &operator=(const String &other)
-    {
-        if (this != &other)
-            String(other).swap(*this);
-        return *this;
-    }
-    
-    void swap(String & a) {
-        std::swap(size, a.size);
-        std::swap(str, a.str);
-    }
-    
-    void append(String &other) {
-        size_t newSz = size + other.size;
-        char *newStr = new char[newSz + 1];
-        newStr[newSz] = '\0';
+        String operator[](unsigned int to) const;
         
-        const char * i = str;
-        while (i != str + size) {
-            newStr[i - str] = *i;
-            i++;
-        };
+        PreparedString(const String& source_in, unsigned int from_in);
         
-        i = other.str;
-        while (i != other.str + other.size) {
-            newStr[size + i - other.str] = *i;
-            i++;
-        };
-        
-        delete [] str;
-        size = newSz;
-        str = newStr;
-    }
-    
-    size_t lengthOfStr(const char * const str) {
-        const char * i = str;
-        while (*i != '\0') {
-            i++;
-        };
-        return i - str;
-    }
-    
-    char* cpStr(const char * const str, size_t strSz) {
-        char *newStr = new char[strSz + 1];
-        newStr[strSz] = '\0';
-        
-        const char * i = str;
-        while (i != str + strSz) {
-            newStr[i - str] = *i;
-            i++;
-        };
-        
-        return newStr;
-    }
-    
-    char & at(size_t idx)       {
-        return str[idx];
-    }
-    char   at(size_t idx) const {
-        return str[idx];
-    }
-    
-    String(const char *str, size_t len) {
-        size = len;
-        this->str = cpStr(str, size);
-    }
-    struct OperatorBracketHelper {
-        OperatorBracketHelper(char *str, size_t i) {
-            str_ = str;
-            i_ = i;
-        }
-        String operator []( size_t j ) const {
-            String s(str_ + i_, j-i_);
-            return s;
-        }
-        char *str_;
-        size_t i_;
+    private:
+        const String& source;
+        unsigned int from;
     };
-    OperatorBracketHelper operator []( size_t i ) const {
-        OperatorBracketHelper o(str, i);
-        return o;
-    }
+    
+    void write(std::ostream& out) const;
+    
+    PreparedString operator[](unsigned int from) const;
     
     size_t size;
     char *str;
 };
+ 
+void String::PreparedString::write(std::ostream& out) const
+{
+    out << (this->source.str+this->from) << std::endl;
+}
+ 
+String String::PreparedString::operator[](unsigned int to) const
+    {
+        char *tmp_c_str = new char[to-from+1];
+        for(char *at_src = source.str+from, *at_dst = tmp_c_str, *last = source.str+to; at_src != last; ++at_src, ++at_dst)
+            *at_dst = *at_src;
+        *(tmp_c_str+to-from) = '\0';
+        String tmp_string(tmp_c_str);
+        delete[] tmp_c_str;
+        
+        return tmp_string;
+    }
+    
+String::PreparedString::PreparedString(const String& source_in, unsigned int from_in): source(source_in), from(from_in)
+{
+}
+ 
+String::PreparedString String::operator[](unsigned int from) const
+{
+    return String::PreparedString(*this,from);
+}
+ 
+void String::write(std::ostream& out) const
+{
+    out << this->str << std::endl;
+}
